d3b5c57a082a0e25c70939f5247f04ad2a28b393
wkbin <2696709667@qq.com>
2025-04-10 10:49:02 +0800

    版本 v2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "AdbFileManager"
-            packageVersion = "2.1.0"
+            packageVersion = "2.2.0"
             description = "FileManager For Desktop"
             copyright = "Copyright 2023 Kebin Wang. All rights reserved."
             vendor = "Kebin Wang"
