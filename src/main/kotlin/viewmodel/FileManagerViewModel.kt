6212d6905e7a95499b3a5f5903674192cb056472
wkbin <2696709667@qq.com>
2025-05-20 10:31:34 +0800

    版本 v2.6.0

--- src/main/kotlin/viewmodel/FileManagerViewModel.kt
+++ src/main/kotlin/viewmodel/FileManagerViewModel.kt
@@ -44,7 +44,7 @@ class FileManagerViewModel(
 ) {
     companion object {
         const val GITHUB_URL = "https://github.com/wkbin/AdbFileManager"
-        const val VERSION = "2.5.1"
+        const val VERSION = "2.6.0"
     }
 
     // Current directory path components
