99bc2f6651c2b8e07c6d0249603e11f786340963
王克斌 <wangkebin1997@gmail.com>
2026-08-21 14:02:14 +0800

    Release 3.1.1

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 import java.util.Properties
 
 object Constants {
-    const val VERSION: String = "3.1.0"
+    const val VERSION: String = "3.1.1"
     const val GITHUB_URL = "https://github.com/wkbin/AdbFileManager"
 }
